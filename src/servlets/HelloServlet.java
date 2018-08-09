1c70f54f03c2916658269306004784cc32619776
mateusz mazur <matmazur90@gmail.com>
2018-08-09 14:55:09 +0200

    first commit

---
+++ src/servlets/HelloServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        resp.getWriter().println("GREETINGS MASSIVE");
+        resp.getWriter().println("Tomcat works rather proper way");
+        resp.getWriter().println("Test do I have to even reload anything");
+        resp.getWriter().println("Test2: I don't even have to rebuild a thing. Not so shabby at all.");
+
+
+
+
+    }
+}
