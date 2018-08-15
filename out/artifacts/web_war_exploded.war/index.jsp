16a5cc18e7051af53edd104d997a71f7d193120d
mateusz mazur <matmazur90@gmail.com>
2018-08-15 21:02:18 +0200

    filled first filter

--- out/artifacts/web_war_exploded.war/index.jsp
+++ out/artifacts/web_war_exploded.war/index.jsp
@@ -4,30 +4,11 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
 <head>
-    <title></title>
+    <title>Hello</title>
 </head>
 <body>
-<div>
-    <h1><a href="${pageContext.request.contextPath}/add-user">Add user</a></h1>
-    <h1><a href="${pageContext.request.contextPath}/hello-servlet">Hello</a></h1>
-    <h1><a href="${pageContext.request.contextPath}login.jsp">Login</a></h1>
-    <h1><a href="${pageContext.request.contextPath}/admin.jsp">Admin login</a></h1>
-
-
-    <div>
-
-            <% List<AdvancedUser> list = (List<AdvancedUser>) session.getAttribute("users"); %>
-
-            <%if (list!=null){%>
-            <%for (AdvancedUser user:list){%>
-            <%if (user!=null)%>
-        <p><%=user%>
-        </p>
-            <%}%>
-            <%}%>
-
-        <div>
-        </div>
+<h2>Hello, your IP address is <%=request.getRemoteAddr()%></h2>
+<a href="admin.jsp">Admin panel</a>
 </body>
 </html>
 
--- src/filters/IpRequestFilter.java
+++ src/filters/IpRequestFilter.java
@@ -1,4 +1,49 @@
 package filters;
 
-public class IpRequestFilter {
+
+import com.sun.deploy.net.HttpResponse;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+
+public class IpRequestFilter implements Filter {
+
+    private Logger logger = Logger.getLogger(IpRequestFilter.class.getName());
+    private String ipPattern;
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        ipPattern = filterConfig.getInitParameter("ipPattern");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+
+
+        String reqIp = servletRequest.getRemoteAddr();
+       logger.warning("Ip filter " + reqIp);
+
+       if (reqIp.matches(ipPattern)){
+           logger.warning("IP OK");
+           filterChain.doFilter(servletRequest,servletResponse);
+       }
+       else {
+           logger.warning("IP NOT OK");
+           HttpServletResponse response = (HttpServletResponse) servletResponse;
+           response.sendError(403);
+       }
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
