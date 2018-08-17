d8cc61a8b6b4d526d561233a137ad32b4f33a08a
mateusz mazur <matmazur90@gmail.com>
2018-08-18 00:21:13 +0200

    corrected servlet code, added second option to choose - ToDo jsp file getting the data

--- src/servlets/SqlServlet.java
+++ src/servlets/SqlServlet.java
@@ -9,53 +9,88 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @WebServlet("/sql-servlet")
 public class SqlServlet extends HttpServlet {
 
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    String param = req.getParameter("value");
+        String param = req.getParameter("value");
 
-    if (param.equals("all")){
+        if (param.equals("all")) {
 
+            List<City> cities= getCityList(resp);
+            req.setAttribute("Cities",cities);
+            req.getRequestDispatcher("/cities-list.jsp").forward(req,resp);
 
-        final String driver = "com.mysql.jdbc.Driver";
+        } if (param.equals("names")) {
+            List<String> names= getCityList(resp)
+                    .stream()
+                    .map(x->x.getName())
+                    .collect(Collectors.toList());
+            req.setAttribute("Cities",names);
+            req.getRequestDispatcher("/cities-list.jsp").forward(req,resp);
+        } else {
+            resp.sendError(403);
+        }
+    }
+
+    private List<City> getCityList(HttpServletResponse resp) throws IOException {
+
+        Statement statement;
+        ResultSet result ;
+        Connection conn;
+        List<City> cities = new ArrayList<>();
 
         try {
+            String driver = "com.mysql.jdbc.Driver";
             Class.forName(driver);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
-        List<City> cities= null;
-        final String dbPath = "jdbc:mysql://localhost:3306/world?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         try {
-            Connection conn = DriverManager.getConnection(dbPath, "root", "pass");
-            Statement statement = conn.createStatement();
+            String dbPath = "jdbc:mysql://localhost:3306/world?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            conn = DriverManager.getConnection(dbPath, "root", "pass");
+            statement = conn.createStatement();
 
             String sqlQuery = "Select * from city";
 
-            ResultSet result=statement.executeQuery(sqlQuery);
+            result = statement.executeQuery(sqlQuery);
 
-                    while (result.next()){
-                        resp.getWriter().println(result.getString("Name")+" " + result.getInt("Population"));
-                    }
+            while (result.next()) {
+                String cityName = result.getString("Name");
+                int population = result.getInt("Population");
+                if (cityName!=null) {
+                    cities.add(new City(cityName, population));
+                }
+            }
 
+            close(statement, result, conn);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
-
+        return cities;
     }
 
 
-
+    private void close(Statement statement, ResultSet result, Connection conn) throws SQLException {
+        if (statement != null) {
+            statement.close();
+        }
+        if (result != null) {
+            result.close();
+        }
+        if (conn != null) {
+            conn.close();
+        }
     }
 }
+
+
 
