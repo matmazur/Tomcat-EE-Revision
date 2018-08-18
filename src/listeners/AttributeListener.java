9276745801627ca6c1dd21ec7eb4f58f467532d3
mateusz mazur <matmazur90@gmail.com>
2018-08-18 16:46:56 +0200

    completed AttributeListener class

--- src/listeners/AttributeListener.java
+++ src/listeners/AttributeListener.java
@@ -1,4 +1,59 @@
 package listeners;
 
-public class AttributeListener {
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class AttributeListener implements HttpSessionAttributeListener,
+        ServletContextAttributeListener, ServletRequestAttributeListener {
+
+
+    @Override
+    public void attributeAdded(ServletContextAttributeEvent servletContextAttributeEvent) {
+
+    }
+
+    @Override
+    public void attributeRemoved(ServletContextAttributeEvent servletContextAttributeEvent) {
+
+    }
+
+    @Override
+    public void attributeReplaced(ServletContextAttributeEvent servletContextAttributeEvent) {
+
+    }
+
+    @Override
+    public void attributeAdded(ServletRequestAttributeEvent servletRequestAttributeEvent) {
+        System.out.println("Request Attribute added " + servletRequestAttributeEvent.getName());
+
+    }
+
+    @Override
+    public void attributeRemoved(ServletRequestAttributeEvent servletRequestAttributeEvent) {
+        System.out.println("Request Attribute removed " + servletRequestAttributeEvent.getName());
+    }
+
+    @Override
+    public void attributeReplaced(ServletRequestAttributeEvent servletRequestAttributeEvent) {
+        System.out.println("Request Attribute replaced " + servletRequestAttributeEvent.getName());
+
+    }
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent httpSessionBindingEvent) {
+
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent httpSessionBindingEvent) {
+
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent httpSessionBindingEvent) {
+
+    }
 }
