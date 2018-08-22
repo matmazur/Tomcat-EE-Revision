f6ef5ae5547bc7f47715ae97c19956dbe1080ab2
mateusz mazur <matmazur90@gmail.com>
2018-08-22 13:35:20 +0200

    created BookServlet cooping with BookDao

--- src/servlets/BookServlet.java
+++ src/servlets/BookServlet.java
@@ -1,4 +1,61 @@
 package servlets;
 
-public class BookServlet {
+import dao.BookDao;
+import model.Book;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.HttpCookie;
+
+
+@WebServlet("/book-servlet")
+public class BookServlet extends HttpServlet {
+
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8");
+        String isbn = req.getParameter("isbn");
+        String title = req.getParameter("title");
+        String description = req.getParameter("description");
+        String option = req.getParameter("option");
+        BookDao dao = new BookDao();
+        Book book = null;
+        String operation = null;
+
+        boolean result = false;
+        if ("search".equals(option)) {
+            book = dao.read(isbn);
+            result = book != null;
+            operation = "search";
+        } else if ("add".equals(option)) {
+            book = new Book(isbn, title, description);
+            result = dao.create(book);
+            operation = "add";
+        } else if ("update".equals(option)) {
+            book = new Book(isbn, title, description);
+            result = dao.update(book);
+            operation = "update";
+        } else if ("delete".equals(option)) {
+            book = new Book(isbn, title, description);
+            result = dao.delete(book);
+            operation = "delete";
+        }
+        ForwardIfBookIsValidAndResultIsTrue(req, resp, book, operation, result);
+    }
+
+    private void ForwardIfBookIsValidAndResultIsTrue(HttpServletRequest req, HttpServletResponse resp, Book book, String operation, boolean result) throws ServletException, IOException {
+        if (book != null && result) {
+            req.setAttribute("option", operation);
+            req.setAttribute("book", book);
+            req.getRequestDispatcher("result.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("error.jsp").forward(req, resp);
+        }
+    }
+
 }
