2a93fa846ec795c540abc3c7c49ad02cd6e0ec89
mateusz mazur <matmazur90@gmail.com>
2018-08-23 12:50:58 +0200

    added class User

---
+++ src/model/User.java
@@ -0,0 +1,44 @@
+package model;
+
+public class User {
+
+    private String pesel;
+    private String firstname;
+    private String lastname;
+
+
+    public User(String pesel, String firstname, String lastname) {
+        this.pesel = pesel;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+
+    public User() {
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
+
